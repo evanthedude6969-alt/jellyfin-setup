5a38c1b2c8dfcb7f78a169d22ec77807bc0a7df8
evanthedude6969-alt <evanthedude6969@gmail.com>
2026-07-20 01:36:19 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+function login() {
 
+    let password = document.getElementById("password").value;
+
+    if(password === "1234"){
+
+        window.location.href = "home.html";
+
+    }else{
+
+        document.getElementById("error").innerHTML =
+        "Incorrect password.";
+
+    }
+
+}
